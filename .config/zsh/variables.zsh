fe804526f66208de6b4219cf92b1b2016e14a338
Bruno Adelé <brunoadele@gmail.com>
2021-03-16 23:04:36 +0100

    Move GPG to XDG_CONFIG_HOME folder

--- .config/zsh/variables.zsh
+++ .config/zsh/variables.zsh
@@ -16,7 +16,18 @@ export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 
+# XDG Folders (https://wiki.archlinux.org/index.php/XDG_Base_Directory#Supported)
+export XDG_CONFIG_HOME=${HOME}/.config
+export XDG_CACHE_HOME=${HOME}/.cache
+export XDG_DATA_HOME=${HOME}/.local/share
+export GNUPGHOME=${XDG_CONFIG_HOME}/gnupg
+
 # GPG
 export SSH_AUTH_SOCK=$(gpgconf –list-dirs agent-ssh-socket)
-export GPG_USERID=DBC1BED7F89CDAFA81D1E25018A8AF963E10FCD4
-export GPG_BACKUP_DIR=/mnt/usb-black-disk/freefilesync/famille/bruno/home/gpg
+export GPG_USERID=00F421C4C5377BA39820E13F6B95E13DE469CC5D
+export GPG_BACKUP_DIR=/mnt/usb-black-disk/freefilesync/famille/bruno/home/security/gpg
+
+# Private folderss
+export WORK=/home/$HOME/work/projects
+export PRIVATE=/home/$HOME/private/projects
+export PASSWORD_STORE_DIR=${PRIVATE}/pass
