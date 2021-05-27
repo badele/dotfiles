980adcec8ae3aecc9c5132267a022447fe976b03
Bruno Adelé <brunoadele@gmail.com>
2021-05-27 22:50:48 +0200

    Use Adwaita-dark

--- .config/zsh/15-variables.zsh
+++ .config/zsh/15-variables.zsh
@@ -21,11 +21,13 @@ export XDG_CONFIG_HOME=${HOME}/.config
 export XDG_CACHE_HOME=${HOME}/.cache
 export XDG_DATA_HOME=${HOME}/.local/share
 export GNUPGHOME=${XDG_CONFIG_HOME}/gnupg
+export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
 
 # GPG
 export SSH_AUTH_SOCK=$(gpgconf –list-dirs agent-ssh-socket)
 export GPG_USERID=00F421C4C5377BA39820E13F6B95E13DE469CC5D
 export GPG_BACKUP_DIR=/mnt/usb-black-disk/freefilesync/famille/bruno/home/security/gpg
+export TELEPORT_USE_LOCAL_SSH_AGENT DEFAULT=false
 
 # Private folderss
 export WORK=${HOME}/work/projects
