aef8389d8670c25ea7dd689ba4ced6f3d796c413
Bruno Adelé <brunoadele@gmail.com>
2021-05-30 22:58:48 +0200

    Update showing palette

--- .config/zsh/80-alias.zsh
+++ .config/zsh/80-alias.zsh
@@ -13,8 +13,23 @@ alias zs="zinit zstatus"
 alias cdw='cd $WORK'
 alias cdp='cd $PRIVATE'
 
-#alias colortest="msgcat --color=test'
-alias colortest="tpal art spaceinvaders; tpal art yinyang; tpal minimat; wpg --preview"
+# Trash
+alias rm='echo "use trash-put instead, if you would like use rm, use \\ rm"; false'
+alias trm='trash-put'
+alias tls='trash-list'
+alias tre='trash-restore'
+alias tem='trash-empty'
+
+# Cloud & co
+alias a=aws
+alias g=gcloud
+alias k=kubectl
+alias h=helm
+
+# alias colorpal="wpg --preview"
+# alias colorpal="msgcat --color=test"
+alias colortest='cat $HOME/.config/color_palete/colors_palete.ansi'
+
 alias vim=nvim
 
 # Date & Time
