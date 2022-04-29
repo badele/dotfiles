2bb62349d5ef3effe981b5c3d28a6ea8cd6c0a24
Bruno Adelé <brunoadele@gmail.com>
2022-04-29 17:23:13 +0200

    Add a [SUPPR] ZSH bindkey

--- .config/zsh/90-bindkeys.zsh
+++ .config/zsh/90-bindkeys.zsh
@@ -10,15 +10,16 @@
 # Main commands in `man zshzle` & `zle -al`
 
 # Shell
-bindkey "^[[1;5D" 	backward-word		            # [CTRL-LEFT]		Move backward one word
-bindkey "^[[1;5C" 	forward-word		            # [CTRL-RIGHT]		Move forward one word
-bindkey "^L" 		clear-screen		            # [CTRL-L]		    Reset screen
-bindkey "^A" 		beginning-of-line	            # [CTRL-A]		    Beginning of line
-bindkey "^E" 		end-of-line		                # [CTRL-E]		    End of line
-bindkey "^[[F" 		end-of-line           	        # [End]			    Go to end of line
-bindkey "^[[H" 		beginning-of-line      	        # [Home]		    Go to beginning of line
+bindkey "^[[1;5D" 	backward-word		                # [CTRL-LEFT]		    Move backward one word
+bindkey "^[[1;5C" 	forward-word		                # [CTRL-RIGHT]		  Move forward one word
+bindkey "^L" 		    clear-screen		                # [CTRL-L]		      Reset screen
+bindkey "^A" 		    beginning-of-line	              # [CTRL-A]		      Beginning of line
+bindkey "^E" 		    end-of-line		                  # [CTRL-E]		      End of line
+bindkey "^[[F" 		  end-of-line           	        # [End]			        Go to end of line
+bindkey "^[[H" 		  beginning-of-line      	        # [Home]		        Go to beginning of line
 bindkey "^[[A"      history-substring-search-up     # [Up]              Search up substring history
 bindkey "^[[B"      history-substring-search-down   # [Down]            Search down substring history
+bindkey "^[[3~"     delete-char                     # [SUPPR]           Delete a character
 
 # FZF & Z
 bindkey "^R" 		fzf-history-widget	            # [CTRL-R]		Show FZF histories
